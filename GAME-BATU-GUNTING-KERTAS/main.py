ae6b2a06c8775b6b6ed961a12b3cce763cf22be1
habsyi <brillyan29806@gmail.com>
2026-07-24 21:52:49 +0700

    Delete GAME-BATU-GUNTING-KERTAS directory

--- GAME-BATU-GUNTING-KERTAS/main.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# menyiapkan pilihan player dan computer
-pilihan = ['gunting', 'batu', 'kertas']
-input_player = input("Masukkan pilihan kamu (gunting, batu, atau kertas): ").lower()
-input_computer = random.choice(pilihan)
-
-print(f"pilihan kamu: {input_player}")
-print(f"pilihan komputer: {input_computer}")
